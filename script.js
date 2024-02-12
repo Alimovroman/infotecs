06f62689614f56eb102aa45e6d01aa045a821bcc
Roman <Alimov.ramon@gmail.com>
2024-02-12 03:49:58 +0300

    add sort of columns and filter of eyes

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const lastName = document.body.getElementsByClassName("lastName");
 const about = document.body.getElementsByClassName("about");
 const eyeColor = document.body.getElementsByClassName("eyeColor");
 const tableWrapper = document.body.getElementsByClassName("tableWrapper");
+const eyeColorSelect = document.body.getElementsByClassName("eyeColorSelect");
+const visibleFirstName =
+  document.body.getElementsByClassName("visibleFirstName");
 
 let mainData = [...data];
-let totalPages = data.length / 10;
+let totalPages = Math.floor(mainData.length / 10);
 let page = 1;
 let minCount = 0;
 let maxCount = 10;
@@ -17,11 +20,6 @@ const dataOfPageFilter = () => {
   dataOfPage = mainData.filter((e, i) => i >= minCount && i < maxCount);
 };
 
-dataOfPageFilter();
-
-createTable(tbody[0], dataOfPage);
-createElementsInside();
-
 //отрисовываем список пользователей
 function createTable(tbody, array) {
   tbody.innerHTML = "";
@@ -29,13 +27,15 @@ function createTable(tbody, array) {
   array.forEach((item) => {
     let tr = document.createElement("tr");
     let tdFirstName = document.createElement("td");
-    tdFirstName.className = item.id;
     let tdLastName = document.createElement("td");
-    tdLastName.className = "tdLastName";
     let tdAbout = document.createElement("td");
-    tdAbout.className = "tdAbout";
     let tdEyeColor = document.createElement("td");
-    tdEyeColor.className = "tdEyeColor";
+
+    tdFirstName.className = `${item.id} tdFirstName`;
+    tdLastName.className = item.id;
+    tdAbout.className = `${item.id} tdAbout`;
+    tdEyeColor.className = item.id;
+
     tdFirstName.innerText = item.name.firstName;
     tdLastName.innerText = item.name.lastName;
     tdAbout.innerText = item.about;
@@ -47,31 +47,128 @@ function createTable(tbody, array) {
     tr.appendChild(tdEyeColor);
     tbody.appendChild(tr);
 
-    tdFirstName.addEventListener("dblclick", editFieldHandler);
+    tdFirstName.addEventListener("dblclick", (e) =>
+      editFieldHandler(e, "firstName")
+    );
+    tdLastName.addEventListener("dblclick", (e) =>
+      editFieldHandler(e, "lastName")
+    );
+    tdAbout.addEventListener("dblclick", (e) => editFieldHandler(e, "about"));
+    tdEyeColor.addEventListener("dblclick", (e) =>
+      editFieldHandler(e, "eyeColor")
+    );
   });
 }
 
-//редактирование поля
-function editFieldHandler(e) {
-  const id = e.target.className;
-  let editTable = document.createElement("div");
-  editTable.className = "editTable";
-  editTable.innerHTML = `<div><input type='text' class='inputEdit'/><button class='btnSave'>Save</button</div>`;
-  tableWrapper[0].appendChild(editTable);
-
-  saveEditField(id);
+//скрытие колонки
+function showColumn(columnName) {
+  const tdFirstName = document.body.getElementsByClassName("tdFirstName");
+  switch (columnName) {
+    case "firstName":
+      for (let i = 0; i < tdFirstName.length; i++)
+        tdFirstName[i].style.visibility = "hidden";
+  }
 }
 
-function saveEditField(id) {
-  let editTable = document.body.getElementsByClassName("editTable");
+//фильтр глаз
+
+function filterEyes() {
+  const pageBtn = document.body.getElementsByClassName("pageBtn");
+  const countPage = pageBtn.length;
+  page = 1;
+
+  switch (this.value) {
+    case "blue":
+      mainData = data.filter((e) => e.eyeColor === this.value);
+      break;
+    case "red":
+      mainData = data.filter((e) => e.eyeColor === this.value);
+      break;
+    case "green":
+      mainData = data.filter((e) => e.eyeColor === this.value);
+      break;
+    case "brown":
+      mainData = data.filter((e) => e.eyeColor === this.value);
+      break;
+    default:
+      mainData = [...data];
+  }
+
+  for (let i = 0; i < countPage; i++) {
+    pageBtn[0].remove();
+  }
+  totalPages = Math.ceil(mainData.length / 10);
+  changePageHandler(page);
+  createPagination();
+}
+
+//редактирование поля
+function editFieldHandler(e, column) {
+  let id;
+  let mainTeg;
+  if (column === "about") {
+    id = e.target.className.split(" ")[0];
+    mainTeg = `<textarea class='inputEdit' rows='5' cols='40'>${e.target.innerText}</textarea>`;
+  } else if (column === "eyeColor") {
+    id = e.target.className;
+    mainTeg = `<select class='inputEdit'>
+      <option value='blue'>blue</option>
+      <option value='green'>green</option>
+      <option value='brown'>brown</option>
+      <option value='red'>red</option>
+    </select>`;
+  } else {
+    id = e.target.className;
+    mainTeg = `<input type='text' value=${e.target.innerText} class='inputEdit' />`;
+  }
+
+  let editTable = document.createElement("div");
+  editTable.className = "editTable";
+  editTable.innerHTML = `<div><div>Edit the field</div>${mainTeg}<button class='btnSave'>Save</button</div>`;
+  tableWrapper[0].appendChild(editTable);
+
+  saveEditField(id, column);
+}
+
+function saveEditField(id, column) {
+  const editTable = document.body.getElementsByClassName("editTable");
   const btnSave = document.body.getElementsByClassName("btnSave");
   btnSave[0].onclick = function () {
     const inputEdit = document.body.getElementsByClassName("inputEdit");
-    mainData = mainData.map((e) =>
-      e.id === id
-        ? (e = { ...e, name: { ...e.name, firstName: inputEdit[0].value } })
-        : { ...e }
-    );
+
+    switch (column) {
+      case "firstName":
+        mainData = mainData.map((e) =>
+          e.id === id
+            ? (e = { ...e, name: { ...e.name, firstName: inputEdit[0].value } })
+            : { ...e }
+        );
+        break;
+      case "lastName":
+        mainData = mainData.map((e) =>
+          e.id === id
+            ? (e = { ...e, name: { ...e.name, lastName: inputEdit[0].value } })
+            : { ...e }
+        );
+        break;
+      case "about":
+        mainData = mainData.map((e) =>
+          e.id === id ? (e = { ...e, about: inputEdit[0].value }) : { ...e }
+        );
+        break;
+      case "eyeColor":
+        mainData = mainData.map((e) =>
+          e.id === id
+            ? (e = {
+                ...e,
+                eyeColor:
+                  inputEdit[0].options[inputEdit[0].selectedIndex].value,
+              })
+            : { ...e }
+        );
+        break;
+    }
+
     changePageHandler(page);
     editTable[0].remove();
   };
@@ -88,7 +185,7 @@ const changePageHandler = (p) => {
 };
 
 // создаем пагинацию и добавляем event
-function createElementsInside() {
+function createPagination() {
   for (let i = 1; i <= totalPages; i++) {
     let pageDiv = document.createElement("div");
 
@@ -141,7 +238,12 @@ const sortEyeColor = () => {
   changePageHandler(page);
 };
 
+dataOfPageFilter();
+createTable(tbody[0], dataOfPage);
+createPagination();
+eyeColorSelect[0].addEventListener("change", filterEyes);
 firstName[0].addEventListener("click", sortFirstName);
 lastName[0].addEventListener("click", sortLastName);
 about[0].addEventListener("click", sortAbout);
 eyeColor[0].addEventListener("click", sortEyeColor);
+visibleFirstName[0].addEventListener("click", () => showColumn("firstName"));
